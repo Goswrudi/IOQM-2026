d5899fc72c284e7ea8166edc6054169567a97dc2
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-06 16:25:25 +0530

    Imporved the logic

--- basic_mathematic/lec_01.py
+++ basic_mathematic/lec_01.py
@@ -87,15 +87,22 @@ A is subset of B
 '''
 
 import random
+
 list_C = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 list_D = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) 
 
-C = random.randint(1 , 10 )
+C = random.randint(1 , 10)
 D = random.randint(1, 10)
 
-x = random.choice(list_C )
-y = random.choice(list_D )
+x = random.choice(list_C)
+y = random.choice(list_D)
 
-print(f'{C},{x}')
-print(f'{D},{y}')
+#  Convert the numbers to strings, put them in a list, and join with a comma
+combined_C = ",".join([str(C), str(x)])
+combined_D = ",".join([str(D), str(y)])
 
+print(combined_C)  
+print(combined_D)  
+
+def classify():
+    None
