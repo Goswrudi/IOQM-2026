5a88e0b26dadf9b461853fedab9681ce634c9717
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-06 15:26:21 +0530

    problem #1

--- basic_mathematic/lec_01.py
+++ basic_mathematic/lec_01.py
@@ -1,3 +1,13 @@
 # I just completed the Unacademy lectture 01 Sets of basic Mathematics by PJ sir !!
 # Therefore asked Chatgpt to give me some problems which i can solve via python !!
 
+'''
+Problem
+
+Given a list of numbers, print only the unique numbers while preserving their first appearance.
+
+Input
+[5, 3, 5, 2, 3, 8, 8, 1]
+
+'''
+
