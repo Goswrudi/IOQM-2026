48ec4b7fa30acc9ddf964f603b7aba0c51239e3b
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-08 16:26:40 +0530

    New problem

---
+++ basic_mathematic/lec_03.py
@@ -0,0 +1,21 @@
+'''
+🟢 Problem 1 — Find All Solutions
+
+Find all positive integer solutions of:
+
+x+y=20
+
+Write a Python program that prints every possible pair (x, y).
+
+Expected structure:
+
+(1, 19)
+(2, 18)
+...
+(19, 1)
+Constraint
+
+Don't manually write the answers. Make Python search for them.
+
+Hint: What values can x take?
+'''
