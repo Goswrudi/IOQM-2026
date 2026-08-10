8709ff10c0c2a0b73f7c5620fef79c8ad6cf3e1c
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-10 22:15:34 +0530

    defined the problem

---
+++ basic_mathematic/lec_04.py
@@ -0,0 +1,30 @@
+'''
+
+AP: Airthemtical Progression 
+
+'''
+
+# Probelm # 1:
+
+'''
+🟢 Level 1 — AP Detector
+Problem 1 — Is it an AP?
+
+Write:
+
+is_ap(sequence)
+
+that returns True if the sequence is an AP and False otherwise.
+
+Examples:
+
+[2, 5, 8, 11, 14] → True
+[3, 7, 11, 15] → True
+[1, 2, 4, 8] → False
+
+Bonus: Return the common difference if it is an AP.
+
+'''
+
+def is_ap():
+    None
