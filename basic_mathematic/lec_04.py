0890a09d544c48750c3f7e8a67f4871051f0e073
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-10 22:44:43 +0530

    Solved the First problem

--- basic_mathematic/lec_04.py
+++ basic_mathematic/lec_04.py
@@ -26,5 +26,21 @@ Bonus: Return the common difference if it is an AP.
 
 '''
 
-def is_ap():
-    None
+def is_ap(sequence):
+    x = sequence
+    # y = [3, 7, 11, 15] 
+    # z = [1, 2, 4, 8]
+
+    # two consecutive terms is always a constant value. For example, in the sequence 2, 5, 8, 11, the difference is always constant : Therefore if  d = same , ap is true , else: false   
+    d = x[1] - x[0] # output : 3
+    d2 = x[2] - x[1] # output : 3
+    d3 = x[3] - x[2] # output : 3
+    
+    print(f'{(d)} , {(d2)} , {(d3)} : Therefore D is same (We can say that is this a AP) ')
+
+    if(d == d2) and (d2 == d3):
+        return True
+
+    return False
+
+print(is_ap([2, 5, 8, 11, 14]))    
