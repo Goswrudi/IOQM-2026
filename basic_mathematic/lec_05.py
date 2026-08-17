067e46df6a9596b39c0b619c8dd3299cbaca0238
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-17 19:24:46 +0530

    the algorithm is created

--- basic_mathematic/lec_05.py
+++ basic_mathematic/lec_05.py
@@ -3,3 +3,15 @@
 # The Theorm
 # a|b 
 # b = a*k 
+
+
+# Now Let's Create a Algorithm
+
+a = int(input(f'enter dividend: '))
+b = int(input(f'enter divisor'))
+
+q = a // b
+r = a % b
+
+print(f"The quoteint is {q}")
+print(f"The remainder is {r}")
