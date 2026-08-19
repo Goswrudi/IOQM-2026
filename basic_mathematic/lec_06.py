a4f71dd4b6534af1af023b537c77a7a2ce02aefe
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-19 17:19:02 +0530

    new file & new problem

---
+++ basic_mathematic/lec_06.py
@@ -0,0 +1,15 @@
+# Alright Moving something more 10/10 
+
+# Now we are discovering divisiblity teorms and it's algorithm
+
+'''
+🟢 Level 1 — Warm-up
+
+1. Use the Division Algorithm to write:
+
+157=13q+r
+
+Find q and r.
+'''
+
+
