24879ac76b3dd76d2fe3a65377b74098614e15cb
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-20 18:20:51 +0530

    checked the math physically which is working fine now working on the py script

--- basic_mathematic/lec_07.py
+++ basic_mathematic/lec_07.py
@@ -3,3 +3,36 @@
 
 # Here we go:
 
+'''
+🟡 Level 2 — Understand the theorem
+
+4. Given:
+
+7∣21,7∣35
+
+show that:
+
+7∣(4⋅21−3⋅35)
+
+But don't just calculate the final number. Use the form:
+
+21=7a,35=7b
+
+and factor out 7.
+
+5. Suppose
+
+a∣b
+
+Prove that:
+
+a∣kb
+
+for every integer k.
+
+Tiny proof, but VERY important.
+'''
+
+
+def show():
+    None
