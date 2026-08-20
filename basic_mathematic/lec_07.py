1f05867a2673023ae48dabbf290a22cfdb08b6b5
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-20 17:28:16 +0530

    defined the level 2 in a new filew

---
+++ basic_mathematic/lec_07.py
@@ -0,0 +1,5 @@
+# This is the level 2 of divisblity algorithm and theorm problem solving we are getting some intermediate questions which ask for the logic and IQ instead of "parrot syntax"
+
+
+# Here we go:
+
