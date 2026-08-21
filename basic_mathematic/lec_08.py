1508e3a11cf7763ad90bb10fb196c45a374ffbcb
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-21 18:48:36 +0530

    defined  the questions

---
+++ basic_mathematic/lec_08.py
@@ -0,0 +1,30 @@
+# Alright Now Moving to the level 3 the divisiblity thinking 
+# Now our battle gonnna be legnedry 
+
+'''
+🟠 Level 3 — Divisibility thinking
+
+6. Without calculating the huge number, determine whether
+
+3∣(10
+50
++8)
+
+Hint:
+
+10≡1(mod3)
+
+7. Find the remainder when
+
+10
+20
++10
+5
++13
+
+is divided by 9.
+
+Hint:
+
+10≡1(mod9)
+'''
