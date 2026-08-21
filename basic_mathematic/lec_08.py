184d048d4ea37172d7a3c8d39162cd56583bdb6c
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-21 19:01:41 +0530

    problem 6 is solved

--- basic_mathematic/lec_08.py
+++ basic_mathematic/lec_08.py
@@ -28,3 +28,18 @@ Hint:
 
 10≡1(mod9)
 '''
+
+
+a = (10**50 + 8) % 3
+b = int(input(f'enter divisor (try: 3) : '))
+
+r = a % b
+q = a // b
+
+print(f"The Quouteint is ({q})")
+print(f"The Remainder is ({r})")
+
+if r == 0:
+    print("Result: True (It is perfectly divisible!)")
+else:
+    print("Result: False (There is a remainder!)")
