23d9dee14780c65b4214e35ec756fa06128db945
rudransh goswami <rudranshgoswami415@gmail.com>
2026-08-22 18:11:27 +0530

    defined the algorithm

---
+++ basic_mathematic/lec_09.py
@@ -0,0 +1,5 @@
+# Alrghit So now we moving like the ddragon baby of number thory knowns as Eucldeian Algorithm 
+# Which is all about finding gcd(greatest common divisor) !!
+# we tried to solve this as a automation and manuall way !!!
+
+# Here we go automation way first !
